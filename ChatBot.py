ffb601062d14e217bedaa9d88a3d046a4721eec2
Sadra <sadrarezaei4@gmail.com>
2025-12-03 17:33:30 +0330

    random answer add

--- ChatBot.py
+++ ChatBot.py
@@ -1,6 +1,53 @@
-import streamlit as st
+# import streamlit as st
+# import numpy as np
 
-st.title("Sadra Bot")
+# st.title("Arsalan Bot")
+
+# # Initialize chat history
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
+
+# # Display chat messages from history on app rerun
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
+
+        
+# # React to user input
+# if prompt := st.chat_input("What is up?"):
+#     # Display user message in chat message container
+#     with st.chat_message("user"):
+#         st.markdown(prompt)
+#     # Add user message to chat history
+#     st.session_state.messages.append({"role": "user", "content": prompt})        
+
+# p = np.random.randn(1,30)
+# response = f"SadraBot: {p}"
+# # Display assistant response in chat message container
+# with st.chat_message("assistant"):
+#     st.markdown(response)
+# # Add assistant response to chat history
+# st.session_state.messages.append({"role": "assistant", "content": response}) 
+
+
+import streamlit as st
+import random
+import time
+
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+st.title("Sadra GPT")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -11,19 +58,16 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-        
-prompt = "Hi im SadraBot. How can i help you?"
-# React to user input
+# Accept user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})        
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-response = f"SadraBot: {prompt}"
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    st.markdown(response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": response})    
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        response = st.write_stream(response_generator())
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
